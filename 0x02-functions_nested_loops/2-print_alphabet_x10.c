2a577417cb9e080a6e2e9aad1446ea109bb2784a
AmanyHussein <amanyhussein495@gmail.com>
2023-02-21 13:19:37 -0800

    edits

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
- * print_alphabet_x10 - Check description 
+ * print_alphabet_x10 - Check description
  * Description: prints 10 times the alphabet, in lowercase, followed by a new line.
  * Return: nothing.
  */
