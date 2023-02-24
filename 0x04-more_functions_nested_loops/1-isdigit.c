ca4e917957d7edbd05099cdb1163afdf6ebc85af
AmanyHussein <amanyhussein495@gmail.com>
2023-02-24 05:21:24 -0800

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * _isdigit- check description
+ * @c - input integer character 
+ * description: checks for a digit from 0-9
+ * Return: 1 if c is digit, 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	int i;
+
+	for (i=48; i <= 59; i++)
+	{
+		if (c == i)
+			return (1);
+	}
+	return (0);
+}
