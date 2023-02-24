319f6a802097e1f8d6794957ed3fbe57766ff372
AmanyHussein <amanyhussein495@gmail.com>
2023-02-24 06:05:22 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/** print_numbers - check description
+ * description: print numbers from 0 to 9 followed y a new line
+ * return: nothing 
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar('\n');
+}
