602c723547b1d62ce806350c4ea8ba85ba410196
AmanyHussein <amanyhussein495@gmail.com>
2023-02-24 07:04:35 -0800

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * print_line - check description
+ * @n - input character - number of characters
+ * description: draws a straight line in the terminal
+ * return: nothing
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('_');
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
