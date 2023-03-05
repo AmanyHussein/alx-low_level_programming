48faf1b6f0a57da86c56302796cc0e486f7d0efc
AmanyHussein <amanyhussein495@gmail.com>
2023-03-05 11:50:41 -0800

    Task 3 edits

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,10 @@
 #include"main.h"
 void _puts(char *str)
 {
-	int i = 0;
-	
-	while (str[i] != '\0')
+	while (*str != '\0')
 	{
-		char l;
-		l = str[i];
-		_putchar(l);
-		i++;
+		_putchar(*str);
+		str++;
 	}
 	_putchar('\n');
 
