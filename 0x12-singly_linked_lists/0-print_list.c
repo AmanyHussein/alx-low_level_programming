80de5e24e8a29cfe0542c242438d1e12de1a9d54
AmanyHussein <amanyhussein495@gmail.com>
2023-03-30 03:58:55 -0700

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include"lists.h"
+
+/**
+ * size_t print_list- see description
+ * @list_t *h :singly linked list node
+ * description: prints all the elements of a list_t list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+	const list_t *current = h;
+
+	while(current != NULL)
+	{
+		if(current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			current = current-> next;
+		}
+		printf("[%d]", current->len);
+		printf("%s\n", current->str);
+		current = current->next;
+		i++;
+	}
+
+	return (i);
+
+}
+
